99cb9500b2932db1057c8e8fa952ec0112e25253
Basem Jaber <basemjaber32@gmail.com>
2019-09-10 10:25:51 -0700

    logic has been completed, need to add CSS and Images to complete the game look

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,78 @@
 //psuedo code
-// 4 crystals with random nubers
+// 4 crystals with random numbers
 // random number generated after loss or win
 //when clicking crystal it should be adding with previous value
 //when total is equal player wins, if they go over computer wins
 // everything found online shows using numbers 1-12 with the crystal value
 //restart game without refreshing page/
-$(document).ready (function () {
+var random = 0;
+var losses = 0;
+var wins = 0;
+var comtNumber = 0;
+var playerScore = 0;
 
-    var random;
-    var losses;
-    var wins;
-    var comtNumber;
-
-    comtNumber = Math.floor(Math.random()*99)+25;
+function generateComtNumber() {
+    comtNumber = Math.floor(Math.random() * 99) + 25;
     console.log(comtNumber);
     $(".computerGen").html("Target Number: " + comtNumber);
 
+
+}
+
+function crystalNumberGenerator() {
+    $('.crystals').empty();
+    // $('.crystals').prepend('<div>TEST</div>')
     for (var i = 0; i < 4; i++) {
-        var randomNumber = Math.floor(Math.random() *12)+1;
-        
-        var crystalLoop =$("<div>");
+        var randomNumber = Math.floor(Math.random() * 12) + 1;
+        var crystalLoop = $("<div>");
         $(".crystals").append(crystalLoop);
         $(crystalLoop).attr({
             "cardValue": randomNumber,
-            "class":'crystal'
+            "class": 'crystal'
         });
     };
+}
 
-    $(".crystal").on("click",function(){
-    console.log($(this).attr("cardValue"));
+function resetGame() {
+    generateComtNumber();
+    crystalNumberGenerator();
+    playerScore = 0;
 
-    })
+}
 
+$(document).ready(function () {
 
+    generateComtNumber();
+    crystalNumberGenerator();
 
+    $(".crystals").on("click", '.crystal', function () { // event delegation
+        let cardValue = parseInt($(this).attr("cardValue"));
+        playerScore += cardValue;
+        console.log(playerScore);
 
+        if (playerScore === comtNumber) {
+            wins++;
+            console.log("you win!");
+            resetGame();
+            winsCount();
+        } 
+        
+        else if (playerScore > comtNumber) {
+            losses++;
+            alert("try again! ");
+            resetGame();
+            lossesCount();
+
+        }
+    });
+
+    function winsCount(){
+     $(".wins").html("player wins : " + wins); 
+    };
+    function lossesCount(){
+        $(".loss").html( "losses : " +losses);
+    };
+
+    
 
 });
