cdc8a2c33f1caf1e72c592eefc023d235a45fed6
Basem Jaber <basemjaber32@gmail.com>
2019-09-10 20:16:27 -0700

    all functionality completed, need to add css and touch up a few things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,42 +5,42 @@
 //when total is equal player wins, if they go over computer wins
 // everything found online shows using numbers 1-12 with the crystal value
 //restart game without refreshing page/
-var random = 0;
-var losses = 0;
-var wins = 0;
-var comtNumber = 0;
-var playerScore = 0;
-
-function generateComtNumber() {
-    comtNumber = Math.floor(Math.random() * 99) + 25;
-    console.log(comtNumber);
-    $(".computerGen").html("Target Number: " + comtNumber);
-
-
-}
-
-function crystalNumberGenerator() {
-    $('.crystals').empty();
-    // $('.crystals').prepend('<div>TEST</div>')
-    for (var i = 0; i < 4; i++) {
-        var randomNumber = Math.floor(Math.random() * 12) + 1;
-        var crystalLoop = $("<div>");
-        $(".crystals").append(crystalLoop);
-        $(crystalLoop).attr({
-            "cardValue": randomNumber,
-            "class": 'crystal'
-        });
-    };
-}
-
-function resetGame() {
-    generateComtNumber();
-    crystalNumberGenerator();
-    playerScore = 0;
-
-}
-
 $(document).ready(function () {
+    var random = 0;
+    var losses = 0;
+    var wins = 0;
+    var comtNumber = 0;
+    var playerScore = 0;
+
+
+    function generateComtNumber() {
+        comtNumber = Math.floor(Math.random() * 99) + 25;
+        console.log(comtNumber);
+        $(".computerGen").html("Target Number: " + comtNumber);
+
+
+    }
+
+    function crystalNumberGenerator() {
+        $('.crystals').empty();
+        for (var i = 0; i < 4; i++) {
+            var randomNumber = Math.floor(Math.random() * 12) + 1;
+            var crystalLoop = $("<img>");
+            $(".crystals").append(crystalLoop);
+            $(crystalLoop).attr({
+                "cardValue": randomNumber,
+                "class": 'crystal', 
+                "src": "assets/images/homerSimpson.jpg"
+            });
+        };
+    }
+
+    function resetGame() {
+        generateComtNumber();
+        crystalNumberGenerator();
+        playerScore = 0;
+        $(".score").html("Player Score : " + playerScore);
+    }
 
     generateComtNumber();
     crystalNumberGenerator();
@@ -49,14 +49,15 @@ $(document).ready(function () {
         let cardValue = parseInt($(this).attr("cardValue"));
         playerScore += cardValue;
         console.log(playerScore);
+        $(".score").html("Player Score : " + playerScore);
 
         if (playerScore === comtNumber) {
             wins++;
             console.log("you win!");
             resetGame();
             winsCount();
-        } 
-        
+        }
+
         else if (playerScore > comtNumber) {
             losses++;
             alert("try again! ");
@@ -65,14 +66,11 @@ $(document).ready(function () {
 
         }
     });
-
-    function winsCount(){
-     $(".wins").html("player wins : " + wins); 
+    function winsCount() {
+        $(".wins").html("player wins : " + wins);
     };
-    function lossesCount(){
-        $(".loss").html( "losses : " +losses);
+    function lossesCount() {
+        $(".loss").html("losses : " + losses);
     };
-
-    
 
 });
